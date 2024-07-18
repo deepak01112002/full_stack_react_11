daa06986daf41441d9af2302884e334ad8110d01
deepak01112002 <deepak8574757609@gmail.com>
2024-07-18 12:04:00 +0530

    crud key

--- day7_events/src/Components/Data.jsx
+++ day7_events/src/Components/Data.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
 import DataList from './DataList'
-
+import {v4} from 'uuid'
 function Data() {
     // const [title,setTitle] = useState("");
     // const [price,setPrice] = useState("");
     // const [img,setImg] = useState("");
     // const [des,SetDes] = useState("")
     const [obj,setObj] = useState({
+        id :v4(),
         title : "",
         price : "",
         img : "",
         des : ""
     })
     const [data,setData] = useState([])
-
+    const [dis,setDis] = useState(false)
+    const [id,setId] = useState("")
     const handleChange = (e)=>{
       
         setObj({...obj, [e.target.name] : e.target.value})
@@ -22,12 +24,53 @@ function Data() {
     
     setData([...data,obj])
     setObj({
+         id :v4(),
         title : "",
         price : "",
         img : "",
         des : ""
     })
   }  
+
+   const handleDelete = (dl)=>{
+     
+    let d = data.filter((el)=>{
+      if(el.id != dl){
+        return el;
+      }
+     })
+    setData(d)
+
+   }
+
+   const handleEdit = (ed)=>{
+       setDis(true)
+       setId(ed)
+       data.forEach((el)=>{
+          if(el.id == ed){
+             setObj(el)
+          }
+       })
+   }
+   const handleUpdate = ()=>{
+      let d = data.map((el)=>{
+        if(el.id == id){
+           return {...el,...obj}
+        }else{
+          return el;
+        }
+      })
+      setData(d)
+      setDis(false)
+      setObj({
+        id :v4(),
+       title : "",
+       price : "",
+       img : "",
+       des : ""
+   })
+   }
+   
   return (
     <div>
         <input type="text" value={obj.title} placeholder='Title' name='title' onChange={handleChange} />
@@ -35,7 +78,8 @@ function Data() {
         <input type="text" value={obj.img} placeholder='ImageURL' name='img'  onChange={handleChange} />
         <input type="text" value={obj.des} placeholder='Description' name='des'  onChange={handleChange} />
         <button onClick={handleClick}>Submit</button>
-       {data== null ? <h1>Loading.........</h1> : <DataList arr={data}/>}
+        <button onClick={handleUpdate} style={{ display : dis ? "inline-block" : "none"}}>Update</button>
+       {data== null ? <h1>Loading.........</h1> : <DataList del={handleDelete} arr={data} edit={handleEdit}/>}
     </div>
   )
 }
--- day7_events/src/Components/DataList.jsx
+++ day7_events/src/Components/DataList.jsx
@@ -6,12 +6,12 @@ function DataList(props) {
         {
            props.arr && props.arr.map((el)=>{
                 return (
-                     <div>
-                        <img src={el.img} alt="" />
+                     <div key={el.id}>
+                        <img src={el.img} alt="" width={"150px"} />
                         <h3>{el.title} - ₹{el.price}</h3>
                         <p>{el.des}</p>
-                        <button>Delete</button>
-                        <button>Edit</button>
+                        <button onClick={()=>props.del(el.id)}>Delete</button>
+                        <button onClick={()=>props.edit(el.id)}>Edit</button>
                      </div>
                 )
             })
