d0a2993bf064d7c980007fc32d3f41ec1f1c6132
deepak01112002 <deepak8574757609@gmail.com>
2024-07-24 12:00:21 +0530

    form validation

--- day9_formController/src/App.jsx
+++ day9_formController/src/App.jsx
@@ -6,10 +6,12 @@ import './App.css'
 function App() {
    const [obj,setObj] = useState({
       email : "",
-      password : ""
+      password : "",
+      username : ""
    })
    const [arr,setarr] = useState([])
    const [viewPass,setviewPass] = useState(false)
+   const [pass,setPass] = useState(null)
    const handleChange = (e)=>{
         let {name,value} = e.target
       setObj({...obj,[name] : value})
@@ -19,18 +21,32 @@ function App() {
     setarr([...arr,obj])
     setObj({
       email : "",
-      password : ""
+      password : "",
+      username : ""
     })
    }
-   console.log(arr)
+   const handleBlur =()=>{
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,25}$/; 
+
+     console.log(regex.test(obj.password))
+        // if(obj.password.length >= 8){
+        //   setPass(true)
+        // }
+        
+   }
+   const handleFocus = ()=>{
+    //  console.log("Focus wala"); 
+   }
   return (
     <>
       <form onSubmit={handleSubmit}>
-         <input type="email" value={obj.email} name='email' onChange={handleChange} required/>
-         <input type={viewPass ? "text" : "password"}  name='password' value={obj.password} onChange={handleChange} required/>
-         {viewPass==false ? <span onClick={()=>setviewPass(true)} class="material-symbols-outlined">
+          <input type="text" value={obj.username}  name='username' onChange={handleChange} /><br /><br />
+         <input type="email" value={obj.email} name='email' onChange={handleChange} required/><br /><br />
+         <input type={viewPass ?  "text" : "password"} className={pass == false ? 'a' : ""} onBlur={handleBlur}  name='password' value={obj.password} onChange={handleChange} required/>
+         {pass == false ? <span className='deep'>Password Must be 8 Characters</span> : ""} <br /><br />
+         {viewPass==false ? <span onClick={()=>setviewPass(true)} className="material-symbols-outlined">
           visibility
-            </span> : <span onClick={()=>setviewPass(false)} class="material-symbols-outlined">
+            </span> : <span onClick={()=>setviewPass(false)} className="material-symbols-outlined">
             visibility_off
             </span>}
          <input type="submit" />
