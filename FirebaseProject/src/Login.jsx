6f3ea01f192087594a51a2e98536403af7f657ad
deepak01112002 <deepak8574757609@gmail.com>
2024-09-20 12:16:14 +0530

    adde

--- FirebaseProject/src/Login.jsx
+++ FirebaseProject/src/Login.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { db } from './FirebaseFolder/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
 
 function Login() {
     const [email,setEmail] = useState("");
     const [pass,setPass] = useState("")
     const [data,setData] = useState([])
+    const [i,setI] = useState(false)
+    const [editId,setEditId] = useState(null)
     const UserCollection = collection(db,"users")
     useEffect(()=>{
         async function getData (){
@@ -17,18 +19,50 @@ function Login() {
            setData(val)
         }
         getData()
-    },[])  
+    },[i])  
     const handleSubmit = async (e)=>{
         e.preventDefault()
-        
+        let obj = {
+          email,
+          pass
+        }
+      const data = doc(db,"users",editId)
+      console.log(data)
+      await updateDoc(data,obj)
+      alert("Data updated Successfully")
+        setI(!i)
+    }
+    const handleDelete = async (id)=>{
+       const data = doc(db,"users",id)
+       await deleteDoc(data)
+       alert("Data Deleted Successfully")
+      //  getData()
+      setI(!i)
+    }
+
+    const handleEdit = (id,email,pass)=>{
+       setEmail(email)
+       setPass(pass)
+       setEditId(id)
     }
   return (
     <div>
         <form onSubmit={handleSubmit}>
-          <input type="text" onChange={(e)=>setEmail(e.target.value)} />
-          <input type="text" onChange={(e)=>setPass(e.target.value)}/>
+          <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} />
+          <input type="text" value={pass} onChange={(e)=>setPass(e.target.value)}/>
           <input type="submit" />
       </form>
+      {
+        data.map((el)=>{
+          return (
+              <div>
+                  <h1>{el.email}</h1>
+                  <button onClick={()=>handleEdit(el.deepak,el.email,el.pass)}>Edit</button>
+                  <button onClick={()=>handleDelete(el.deepak)}>Delete</button>
+              </div>
+          )
+        })
+      }
     </div>
   )
 }
